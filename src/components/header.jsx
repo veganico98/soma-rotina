1bdfd67787326e45764e04e70f507012f2e2f3dd
Nicolas <nicolaslinaresoli@gmail.com>
2025-07-07 18:38:41 -0300

    Início header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='bg-sky-950'>
-        Header
+    <div className='h-14 bg-linear-to-t from-white to-sky-600 text-sky-600 grid-cols-2 flex items-center justify-center'>
+        <img src="" alt="" />
+        <h1 className='font-bold'>Soma Rotina</h1>
     </div>
   )
 }

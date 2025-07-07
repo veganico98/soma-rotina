85208824ccaaec95777df508a0a6023993cce866
Nicolas <nicolaslinaresoli@gmail.com>
2025-07-07 20:06:18 -0300

    Ajuste de layout

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,36 +2,32 @@ import React from 'react'
 
 const Formulario = () => {
   return (
-    <div className='flex flex-col items-center justify-center h-screen bg-indigo-950'>
+    <div className='flex flex-col items-center justify-center h-screen bg-indigo-900'>
         <form className='flex flex-col gap-4'>
             <div className='flex flex-col gap-2 bg-white p-6 rounded-lg shadow-lg w-96'>
                 
-                    <label htmlFor='monday' className='text-sky-600 font-bold'>Segunda-feira</label>
-                    <input type='number' id='monday' placeholder='Digite em minutos:' className='border border-sky-600 rounded-mb-2 p2 px-4 focus:outline-none focus:ring-2 focus:ring-sky-600' />
+                    <label htmlFor='monday' className='text-indigo-700 font-bold'>Segunda-feira</label>
+                    <input type='number' id='monday' placeholder='Digite em minutos:' className='border border-indigo-700 rounded-mb-2 p2 px-4 focus:outline-none focus:ring-2 focus:ring-indigo-700' />
 
-                    <label htmlFor='monday' className='text-sky-600 font-bold'>Terça-feira</label>
-                    <input type='number' id='monday' placeholder='Digite em minutos:' className='border border-sky-600 rounded-mb-2 p2 px-4 focus:outline-none focus:ring-2 focus:ring-sky-600' />
+                    <label htmlFor='monday' className='text-indigo-700 font-bold'>Terça-feira</label>
+                    <input type='number' id='monday' placeholder='Digite em minutos:' className='border border-indigo-700 rounded-mb-2 p2 px-4 focus:outline-none focus:ring-2 focus:ring-indigo-700' />
                     
-                    <label htmlFor='monday' className='text-sky-600 font-bold'>Quarta-feira</label>
-                    <input type='number' id='monday' placeholder='Digite em minutos:' className='border border-sky-600 rounded-mb-2 p2 px-4 focus:outline-none focus:ring-2 focus:ring-sky-600' />
+                    <label htmlFor='monday' className='text-indigo-700 font-bold'>Quarta-feira</label>
+                    <input type='number' id='monday' placeholder='Digite em minutos:' className='border border-indigo-700 rounded-mb-2 p2 px-4 focus:outline-none focus:ring-2 focus:ring-indigo-700' />
                 
-
-                
-                    <label htmlFor='monday' className='text-sky-600 font-bold'>Quinta-feira</label>
-                    <input type='number' id='monday' placeholder='Digite em minutos:' className='border border-sky-600 rounded-mb-2 p2 px-4 focus:outline-none focus:ring-2 focus:ring-sky-600' />
+                    <label htmlFor='monday' className='text-indigo-700 font-bold'>Quinta-feira</label>
+                    <input type='number' id='monday' placeholder='Digite em minutos:' className='border border-indigo-700 rounded-mb-2 p2 px-4 focus:outline-none focus:ring-2 focus:ring-indigo-700' />
                     
-                    <label htmlFor='monday' className='text-sky-600 font-bold'>Sexta-feira</label>
-                    <input type='number' id='monday' placeholder='Digite em minutos:' className='border border-sky-600 rounded-mb-2 p2 px-4 focus:outline-none focus:ring-2 focus:ring-sky-600' />
+                    <label htmlFor='monday' className='text-indigo-700 font-bold'>Sexta-feira</label>
+                    <input type='number' id='monday' placeholder='Digite em minutos:' className='border border-indigo-700 rounded-mb-2 p2 px-4 focus:outline-none focus:ring-2 focus:ring-indigo-700' />
                     
-                    <label htmlFor='monday' className='text-sky-600 font-bold'>Sábado</label>
-                    <input type='number' id='monday' placeholder='Digite em minutos:' className='border border-sky-600 rounded-mb-2 p2 px-4 focus:outline-none focus:ring-2 focus:ring-sky-600' />
-                
-
-                            
-                    <label htmlFor='monday' className='text-sky-600 font-bold'>Domingo</label>
-                    <input type='number' id='monday' placeholder='Digite em minutos:' className='border border-sky-600 rounded-mb-2 p2 px-4 focus:outline-none focus:ring-2 focus:ring-sky-600' />
+                    <label htmlFor='monday' className='text-indigo-700 font-bold'>Sábado</label>
+                    <input type='number' id='monday' placeholder='Digite em minutos:' className='border border-indigo-700 rounded-mb-2 p2 px-4 focus:outline-none focus:ring-2 focus:ring-indigo-700' />
+                       
+                    <label htmlFor='monday' className='text-indigo-700 font-bold'>Domingo</label>
+                    <input type='number' id='monday' placeholder='Digite em minutos:' className='border border-indigo-700 rounded-mb-2 p2 px-4 focus:outline-none focus:ring-2 focus:ring-indigo-700' />
                     
-                    <button type='submit' className='bg-purple-700 rounded-r-lg gap-20'>Enviar</button>
+                    <button type='submit' className='bg-indigo-700 rounded-r-lg gap-20'>Enviar</button>
             </div>
         </form>
     </div>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='h-14 bg-linear-to-t bg-indigo-950 grid-cols-2 flex items-center justify-center'>
+    <div className='h-14 bg-linear-to-t bg-indigo-900/90 grid-cols-2 flex items-center justify-center'>
         <img src="" alt="" />
         <h1 className='font-bold dark:text-white'>Soma Rotina</h1>
     </div>

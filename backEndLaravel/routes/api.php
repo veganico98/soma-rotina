5918c3cad69f63b61e98c51965ceaafb61a3ead8
Nicolas <nicolaslinaresoli@gmail.com>
2025-07-14 21:13:43 -0300

    Incluso o arquivo api.php

--- backEndLaravel/routes/api.php
+++ backEndLaravel/routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/teste-api', function () {
-    return response()->json(['message' => 'API funcionando!']);
+Route::post('/resultado', function (Request $request) {
+    $dados = $request->all();
+    return response()->json([
+        'mensagem' => 'Dados recebidos com sucesso!',
+        'dados' => $dados
+    ]);
 });
